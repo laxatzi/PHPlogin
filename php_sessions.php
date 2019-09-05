5781a2bf777a7091c126ad6c6a1a0024ef24679f
laxatzi <laxatzi@yahoo.com>
2019-09-05 16:47:57 +0300

    correct href destination

--- php_sessions.php
+++ php_sessions.php
@@ -23,6 +23,6 @@
    ?>
       <h2>PHP sessions allow you to track people</h2>
 
-      <a href="http://localhost/test_folder/Ch11-php-sessions-2.php">View Session</a>
+      <a href="http://localhost/test_folder/php_sessions-2.php">View Session</a>
 </body>
 </html>
