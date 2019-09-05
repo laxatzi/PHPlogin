04268b29fab7b2cf307e791b89f4660cba5cc3b7
laxatzi <laxatzi@yahoo.com>
2019-09-05 16:41:05 +0300

    maintaining state in php_sessions.php page

--- php_sessions.php
+++ php_sessions.php
@@ -1,4 +1,13 @@
+<?php
+      //PHP Sessions are SuperGlobal variables...a type of array
+      // PHP Sessions allow to track people visiting your website thus ...maintaining state
+      
+      session_start();   //start the session => it tells php engine that we gonna use sessions on this page
+
+
+?>
 <!DOCTYPE html>
+
 <html>
 <head>
 	<title>Ch 11 PHP Sessions</title>
