1612abf24cb17596c85c2e0a79ff170c3854a9cb
laxatzi <laxatzi@yahoo.com>
2019-09-05 15:16:55 +0300

    create custom_funcs php folder to showcase how custom php functions work

---
+++ custom_funcs.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>A custom PHP function</title>
+	
+	</head>
+	<body>
+
+		<h1>PHP Custom Functions - keeping your code clean</h1>
+
+      <!-- PHP code goes -->
+
+	</body>
+</html>
