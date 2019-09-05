6c7163a500acc172255f5e0cca408aeb708b51f4
laxatzi <laxatzi@yahoo.com>
2019-09-05 16:46:01 +0300

    set sessions variables

--- php_sessions.php
+++ php_sessions.php
@@ -1,5 +1,5 @@
 <?php
-      //PHP Sessions are SuperGlobal variables...a type of array
+      //PHP Sessions are SuperGlobal variables...a type of associative  array (array with key/value pairs)
       // PHP Sessions allow to track people visiting your website thus ...maintaining state
       
       session_start();   //start the session => it tells php engine that we gonna use sessions on this page
@@ -14,6 +14,13 @@
 </head>
 
 <body>
+   <?php
+         //set session variables
+         $_SESSION["favcolor"] = "blue";
+         $_SESSION["favanimal"] = "dog";
+
+         echo "Session variables are set!";
+   ?>
       <h2>PHP sessions allow you to track people</h2>
 
       <a href="http://localhost/test_folder/Ch11-php-sessions-2.php">View Session</a>
