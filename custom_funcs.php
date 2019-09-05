7c5f74976a64ed40d68c8c15f54c79df2adf8d36
laxatzi <laxatzi@yahoo.com>
2019-09-05 15:35:49 +0300

    create php custom function

--- custom_funcs.php
+++ custom_funcs.php
@@ -1,3 +1,10 @@
+<?php
+      //php code goes here
+      //inserting a function
+      function greetFriends($name="Panos"){
+         return "Hello $name";
+      }
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -7,8 +14,12 @@
 	<body>
 
 		<h1>PHP Custom Functions - keeping your code clean</h1>
+   <?php
+      // PHP code goes 
+      echo greetfriends("Maria"); // PHP function names are NOT case sensitive
+                                  // browser will display "Hello Maria"
 
-      <!-- PHP code goes -->
+   ?>
 
 	</body>
 </html>
