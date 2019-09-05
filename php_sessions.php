6ef2a31e6f9bba54841b446a8c0c7115d4e2818d
laxatzi <laxatzi@yahoo.com>
2019-09-05 16:22:17 +0300

    add html code in the php_sessions.php file

--- php_sessions.php
+++ php_sessions.php
@@ -1,1 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ch 11 PHP Sessions</title>
+</head>
 
+<body>
+      <h2>PHP sessions allow you to track people</h2>
+
+      <a href="http://localhost/test_folder/Ch11-php-sessions-2.php">View Session</a>
+</body>
+</html>
