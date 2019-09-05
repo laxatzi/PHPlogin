c49e456b4f8bed1c30d9b3d0c8dd0edbd237936d
laxatzi <laxatzi@yahoo.com>
2019-09-05 16:54:42 +0300

    update php_sessions-2.php file

---
+++ php_sessions-2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ch 11 PHP Sessions</title>
+</head>
+
+<body>
+    <h2>View session with print_r</h2>
+    <h2>Use sessions in a variable in a conditional statement!</h2>
+
+</body>
+</html>
+
