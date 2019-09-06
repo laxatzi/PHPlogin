05ed9a770fffba4cf4e98bbb173ec46b1784302a
laxatzi <laxatzi@yahoo.com>
2019-09-06 12:38:06 +0300

    conditional to check the right username

--- login_response.php
+++ login_response.php
@@ -2,5 +2,21 @@
 // Start the session
 session_start();
 
+/* Checking to see if they put in the right username.
 
+In the real world, we would be checking both username and password and 
+we would also be talking to a database that would be storing the usernames and
+passwords. 
+
+.. But alas, we are just noob nerds, so we are doing something simple. 
+
+*/
+   $userName = $_POST["userName"];
+
+   if(trim($userName)=="Jimmy"){
+      $_SESSION["isLoggedIn"]=true;
+      header("Location: protected-page.php");
+   } else{
+      header("Location: login.php?badUserCredentials=true");
+   }
 ?>
