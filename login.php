e6b4c6ba5b3ba75327afcfe0abadd2600911a050
laxatzi <laxatzi@yahoo.com>
2019-09-06 11:34:41 +0300

    inserting basic html code

--- login.php
+++ login.php
@@ -1,1 +1,20 @@
-
+<!DOCTYPE html>
+<html>
+	<head>
+	   <title>Form Processing</title>
+      <link rel="stylesheet" type="text/css" href="styles.css">
+  </head>
+  <body>
+      <nav>
+         <ul>
+            <li><a href="login.php">Home</a></li>
+            <li><a href="protected-page.php">Protected page</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="logout.php">Log Out</a></li>
+         </ul>
+      </nav>
+      <main>
+            <!-- PHP code goes here! -->
+      </main>
+</body>
+</html>
