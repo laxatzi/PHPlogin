14b07d81b96e94c0c9554c8ba3ac5a71ed8dba69
laxatzi <laxatzi@yahoo.com>
2019-09-06 12:49:33 +0300

    add html code into logout.php page

--- logout.php
+++ logout.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html>
+	
+	<title>Form processing - Protected page.</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
 
+<body>
+
+  <nav>
+    <ul>
+         <li><a href="login.php">Home</a></li>
+         <li><a href="protected-page.php">Protected page</a></li>
+         <li><a href="#">Contact</a></li>
+         <li><a href="logout.php">Log Out</a></li>
+    </ul>
+  </nav>
+  <main>
+
+    <p>You have been logged out!</p>
+
+  </main>
+
+	</body>
+</html>
