253c9032cc2627e2a98e4cee28e049b2e6250f91
laxatzi <laxatzi@yahoo.com>
2019-09-06 12:42:38 +0300

    add html code in protected-page.php page

--- protected-page.php
+++ protected-page.php
@@ -1,1 +1,26 @@
+<!DOCTYPE html>
+<html>
 
+  <title>Form processing - Protected page.</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+
+<body>
+
+  <nav>
+      <ul>
+      <li><a href="login.php">Home</a></li>
+      <li><a href="protected-page.php">Protected page</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="logout.php">Log Out</a></li>
+    </ul>
+  </nav>
+
+
+  <main>
+
+    <p>You are now viewing members only content! Aren't you special!</p>
+
+  </main>
+
+	</body>
+</html>
