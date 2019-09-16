35042a54ae1718dc7a224fe835f6fa13ec9b8a7d
laxatzi <laxatzi@yahoo.com>
2019-09-16 11:26:18 +0300

    add comment on login-responseNew file

--- login-responseNew.php
+++ login-responseNew.php
@@ -1,1 +1,14 @@
+<?php
+// Start the session
+session_start();
+    
+    /* Checking to see if they put in the right username.
 
+In the real world, we would be checking both username and password and 
+we would also be talking to a database that would be storing the usernames and
+passwords. 
+
+.. But alas, we are just noob nerds, so we are doing something simple. 
+
+*/
+?>
