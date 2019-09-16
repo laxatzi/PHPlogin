0a7924bd596d74c1de2b7a51e6f462b6daa56beb
laxatzi <laxatzi@yahoo.com>
2019-09-16 12:42:32 +0300

    add html code to logoutNew php file

--- logoutNew.php
+++ logoutNew.php
@@ -8,3 +8,29 @@
    // destroy the session
    session_destroy(); 
 ?>
+
+<!DOCTYPE html>
+<html>
+	
+	<title>Form processing - Logout page.</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+
+<body>
+
+  <nav>
+    <ul>
+         <li><a href="loginNew.php">Home</a></li>
+         <li><a href="protected-pageNew.php">Protected page</a></li>
+         <li><a href="#">Contact</a></li>
+         <li><a href="logoutNew.php">Log Out</a></li>
+    </ul>
+  </nav>
+  <main>
+
+    <p>You have been logged out!</p>
+
+  </main>
+
+	</body>
+</html>
+
