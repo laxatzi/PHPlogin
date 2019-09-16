afc83a2708de5eb90ebd88ab06745b0da9e3a570
laxatzi <laxatzi@yahoo.com>
2019-09-16 12:40:03 +0300

    modify protected-pageNew.php and create logoutNew.php file

--- protected-pageNew.php
+++ protected-pageNew.php
@@ -11,4 +11,29 @@
    }
 ?>
 
+<!DOCTYPE html>
+<html>
 
+  <title>Protected page.</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+
+<body>
+
+  <nav>
+      <ul>
+      <li><a href="loginNew.php">Home</a></li>
+      <li><a href="protected-pageNew.php">Protected page</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="logoutNew.php">Log Out</a></li>
+    </ul>
+  </nav>
+
+
+  <main>
+
+    <p>You are now viewing members only content! Aren't you special!</p>
+
+  </main>
+
+	</body>
+</html>
