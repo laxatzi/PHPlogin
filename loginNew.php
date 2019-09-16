7aa42e4fefbd52fb3dec123299267d3e48f0b3be
laxatzi <laxatzi@yahoo.com>
2019-09-16 10:29:53 +0300

    insert basic html

--- loginNew.php
+++ loginNew.php
@@ -1,1 +1,25 @@
-
+<?php
+   session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Form Processing</title>
+</head>
+<body>
+   <nav>
+      <ul>
+         <li><a href="login.php">Home</a></li> 
+         <li><a href="protected-page.php">Protected Page</a></li> 
+         <li><a href="#">Contact</a></li> 
+         <li><a href="logout.php">Logout</a></li> 
+      </ul>
+   </nav>
+   <main>
+       
+   </main>
+</body>
+</html>
