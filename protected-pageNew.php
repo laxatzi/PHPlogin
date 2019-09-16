6bacb89b664e88692842a886f299d34f75085beb
laxatzi <laxatzi@yahoo.com>
2019-09-16 12:35:57 +0300

    add php code to protected-pageNew.php file section

--- protected-pageNew.php
+++ protected-pageNew.php
@@ -1,1 +1,14 @@
+<?php 
+   //start the session_abort
+   start_session();
+
+   // check session to see if logged in 
+
+   if(isset($_SESSION['isLoggedIn'])){
+
+   }else{
+      header('Location: loginNew.php?isBlock=true');
+   }
+?>
+
 
