25792050b0ad79fff975626d3e8632867c23d6d9
laxatzi <laxatzi@yahoo.com>
2019-11-15 12:06:29 +0200

    add comments to contact.php file

--- contact.php
+++ contact.php
@@ -17,8 +17,9 @@
    // process form
    // get form data
    foreach($form_elements as $element){
-      $form[$element] = '';
+      $form[$element] = htmlspecialchars($_POST[$element]); //htmlspecialchars make sure all characters in the input are valid...this statement is gonna loop through each or our form elements and its gonna set our form array by grab the form data (names) from the form 
    }
+    // the next step is to actually check these form elements
    }
    else{
       foreach($form_elements as $element){
